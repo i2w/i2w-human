4872b4aaa05560d5f7f0dbb57b3088548d4d80aa
Ian White <ian.w.white@gmail.com>
2021-07-05 20:31:34 +1000

    Use object name if it is present

--- lib/i2w/human.rb
+++ lib/i2w/human.rb
@@ -5,7 +5,7 @@ require_relative 'human/version'
 module I2w
   # TODO:
   # - [ ] integrate with i18n
-  # - [ ] display error hashes
+  # - [ ] display error objects
   # - [ ] default handler for objects which invites people to configure
   # - [ ] add configuration ability for extension
   #       
@@ -14,8 +14,9 @@ module I2w
 
     def call(object)
       return object.to_human if object.respond_to?(:to_human)
-
-      object.to_s
+      return object.name if object.respond_to?(:name)
+      
+      object.to_s.humanize
     end
     alias [] call
   end
